f1d56d67ca55cb654135c835005ce48c58d4f6e1
wklein1 <Witali@>
2022-10-22 15:00:34 +0200

    #1 refactor csv_reader, add csv_reader unittest

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import modules.csv.csv_reader as csv_reader
 
-components = csv_reader.readCSV("hardware_components.csv")
+components = csv_reader.read_csv("hardware_components.csv")
--- modules/csv/csv_reader.py
+++ modules/csv/csv_reader.py
@@ -1,14 +1,18 @@
 from csv import DictReader
+from typing import Iterable, Text
 
-def readCSV(file: str)->list:
-    csv_file = open(file,'r', encoding="utf8")
-    csv_reader = DictReader(csv_file)
-
+def parse_csv(csv_file: Iterable[Text])->list:
+    csv_reader = DictReader(f=csv_file)
     table = []
 
     for row in csv_reader:
         table.append(row)
+    return table
 
+def read_csv(file_path: str)->list:
+
+    csv_file = open(file_path,'r', encoding="utf8")
+    table = parse_csv(csv_file)
     csv_file.close()
 
     return table
